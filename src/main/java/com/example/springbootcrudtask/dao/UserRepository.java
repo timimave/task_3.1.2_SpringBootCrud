0fa7a978df3beb965b9a6a081cca623c7c180b22
TIm IM <timimave@gmail.com>
2023-02-21 16:52:29 +0500

    last

--- src/main/java/com/example/springbootcrudtask/dao/UserRepository.java
+++ src/main/java/com/example/springbootcrudtask/dao/UserRepository.java
@@ -2,7 +2,9 @@ package com.example.springbootcrudtask.dao;
 
 import com.example.springbootcrudtask.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
